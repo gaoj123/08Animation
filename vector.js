aa37f4ecf76cc6b4624c432ba63a4ce38645b9fb
Jenny Gao <jgao658@gmail.com>
2018-03-01 13:57:20 -0500

    bounce working

--- vector.js
+++ vector.js
@@ -10,6 +10,71 @@ var stopIt=function(){
     clearInterval(timerId)
 }
 
+var animateDVD=function(){
+    var x=200;
+    var y=225;
+    var randomOne=Math.floor(Math.random()*2);
+    var randomTwo=Math.floor(Math.random()*2);
+    var ranX=Math.floor(Math.random()*7)+1;
+    var ranY=Math.floor(Math.random()*7)+1;
+    var moveRight=true;
+    var moveDown=true;
+    if(randomOne==1){
+	moveRight=true;
+    }
+    else{
+	moveRight=false;
+    }
+    if(randomTwo==1){
+	moveDown=true;
+    }
+    else{
+	moveDown=false;
+    }
+    //window.cancelAnimationFrame(requestID);
+    var rect=function(){
+	clear();
+	//ctx.clearRect(0,0,500,500);
+	//ctx.beginPath();
+	var r=document.createElementNS("http://www.w3.org/2000/svg", "rect");
+	slate.appendChild(r);
+	//ctx.fillStyle="lightsteelblue"
+	if(x>=400){
+	    moveRight=false;
+	}
+	if(x<=0){
+	    moveRight=true;
+	}
+	if(y>=450){
+	    moveDown=false;
+	}
+	if(y<=0){
+	    moveDown=true;
+	}
+	if(moveRight){
+	    x+=ranX;
+	}
+	else{
+	    x-=ranX;
+	}
+	if(moveDown){
+	    y+=ranY;
+	}
+	else{
+	    y-=ranY;
+	}
+	r.setAttribute("x",x);
+	r.setAttribute("y",y);
+	r.setAttribute("width",100);
+	r.setAttribute("height",50);
+	r.setAttribute("fill","lightsteelblue");
+	//ctx.fillRect(x,y,100,50);
+	//requestID=window.requestAnimationFrame(rect);
+    };
+    timerId=setInterval(rect,10);
+    //rect();
+};
+
 var animateCircle=function(){
     var r=0;
     var tooBig=false;
